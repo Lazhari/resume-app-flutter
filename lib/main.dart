48b1db49e10f4a58b7b9f698e10f4bb9f8c74d68
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2018-11-27 23:34:05 +0100

    Initiate the flutter resume app

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: new MainPage());
+  }
+}
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key}) : super(key: key);
+  @override
+  _MainPageState createState() => new _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final _imageHeight = 256.0;
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Stack(
+        children: <Widget>[_buildImage(), _buildTopHeadder()],
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    return new ClipPath(
+      clipper: new DiagonalClipper(),
+      child: new Image.asset('assets/bird.jpg',
+          fit: BoxFit.fitHeight,
+          height: _imageHeight,
+          colorBlendMode: BlendMode.srcOver,
+          color: new Color.fromARGB(120, 20, 10, 40)),
+    );
+  }
+
+  Widget _buildTopHeadder() {
+    return new Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 32.0),
+      child: new Row(
+        children: <Widget>[
+          new Icon(Icons.menu, size: 32.0, color: Colors.white),
+          new Expanded(
+            child: new Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: new Text(
+                "Skills",
+                style: new TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300),
+              ),
+            ),
+          ),
+          new Icon(
+            Icons.linear_scale,
+            color: Colors.white,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class DiagonalClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = new Path();
+    path.lineTo(0.0, size.height - 60.0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0.0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
+}
